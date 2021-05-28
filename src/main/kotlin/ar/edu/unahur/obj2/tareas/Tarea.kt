3e92e93bbb57388d3c390460735c967c0b86501a
Brenda <brenda.denhoff@estudiantes.unahur.edu.ar>
2021-05-27 22:55:32 -0300

    Requerimiento 1

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,14 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+class Tarea(val horasEstimada:Int, val responsable: Empleado){
+    var empleados = mutableListOf<Empleado>()
+
+    fun agregarEmpleado(empleado: Empleado) = empleados.add(empleado)
+
+    fun nominaDeEmpleados() = empleados + responsable
+
+}
+
+class Empleado {
+
+}
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -1,8 +1,27 @@
 package ar.edu.unahur.obj2.tareas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
   describe("Una tarea") {
+      val marcelo = Empleado()
+      val cristian = Empleado()
+      val brian = Empleado()
+      val ezequiel = Empleado()
+
+      val tarea1 = Tarea(50, marcelo)
+      val tarea2 = Tarea(24, ezequiel)
+
+      tarea1.agregarEmpleado(cristian)
+      tarea1.agregarEmpleado(brian)
+      tarea1.agregarEmpleado(ezequiel)
+
+      tarea2.agregarEmpleado(cristian)
+
+      describe("nomina de empleados") {
+          tarea1.nominaDeEmpleados().shouldBe(listOf(cristian, brian, ezequiel, marcelo))
+          tarea2.nominaDeEmpleados().shouldBe(listOf(cristian, ezequiel))
+      }
   }
 })
