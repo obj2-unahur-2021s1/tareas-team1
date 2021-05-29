dbc24e16edc9123620f84281796baa97ba204311
patriciorearte <patriciorearte97@gmail.com>
2021-05-29 18:53:18 -0300

    Requerimiento 2 terminado y testeado

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -4,11 +4,20 @@ class Tarea(val horasEstimada:Int, val responsable: Empleado){
     var empleados = mutableListOf<Empleado>()
 
     fun agregarEmpleado(empleado: Empleado) = empleados.add(empleado)
-
+    fun cantidadDeEmpleados()=empleados.size
     fun nominaDeEmpleados() = empleados + responsable
+
+    //Las horas necesarias para finalizar una tarea son las horas estimadas que requiere divido la cantidad de
+    // empleados que tiene asignados (sin contar al responsable de la misma, que no aporta nada para reducir
+    // este número).
+    fun cuantoTiempoLlevaTerminar()= horasEstimadas.hora /this.cantidadDeEmpleados()
 
 }
 
 class Empleado {
 
+}
+object horasEstimadas
+{
+    val hora =70
 }
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -23,5 +23,11 @@ class TareaTest : DescribeSpec({
           tarea1.nominaDeEmpleados().shouldBe(listOf(cristian, brian, ezequiel, marcelo))
           tarea2.nominaDeEmpleados().shouldBe(listOf(cristian, ezequiel))
       }
+      describe("cuanto tiempo le lleva una tarea")
+      {
+          //Saber cuántas horas se necesitan para finalizar una tarea.
+          tarea1.cuantoTiempoLlevaTerminar().shouldBe(23)
+          tarea2.cuantoTiempoLlevaTerminar().shouldBe(70)
+      }
   }
 })
